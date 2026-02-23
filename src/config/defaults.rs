75bfbe6e95e248573cb59d3ca32c88eed4feb0cf
Alexey <247128645+axkurcom@users.noreply.github.com>
2026-02-23 16:10:39 +0300

    Update defaults.rs

--- src/config/defaults.rs
+++ src/config/defaults.rs
@@ -172,7 +172,7 @@ pub(crate) fn default_proxy_config_reload_secs() -> u64 {
 }
 
 pub(crate) fn default_update_every_secs() -> u64 {
-    12 * 60 * 60
+    2 * 60 * 60
 }
 
 pub(crate) fn default_me_reinit_drain_timeout_secs() -> u64 {
